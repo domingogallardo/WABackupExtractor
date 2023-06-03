0cdddbc5fbeb5cba24d95525154e723b0dc94948
Domingo Gallardo <domingo.gallardo@ua.es>
2023-06-03 10:35:46 +0200

    getChats() returns info about the chat with messages

--- Sources/main.swift
+++ Sources/main.swift
@@ -21,9 +21,14 @@ if let backups = api.getLocalBackups() {
     let mostRecentBackup = backups.sorted(by: { $0.creationDate > $1.creationDate }).first!
     print("Connecting to the most recent backup: \(mostRecentBackup.identifier)")
     api.connectChatStorage(backupPath: mostRecentBackup.path)
-    let chats = api.getChatSessions()
-    for chat in chats {
-        print("Chat: \(chat)")
+    print("Connected to the most recent backup")
+    if let chats = api.getChats() {
+        print("Found \(chats.count) chats")
+        for chat in chats {
+            print(chat)
+        }
+    } else {
+        print("No chats")
     }
 } else {
     print("No local backups")
