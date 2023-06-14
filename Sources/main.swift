121eb6cd2c9cf04b735170a5628ae53948cc7fc7
Domingo Gallardo <domingo.gallardo@ua.es>
2023-06-14 14:01:32 +0200

    Output to file chats.json

--- Sources/main.swift
+++ Sources/main.swift
@@ -13,38 +13,55 @@ let api = WABackup()
 let backups = api.getLocalBackups()
 
 guard backups.count > 0 else {
-    print("No local backups")
+    print("No backups")
     exit(1)
 }
 
-/*
+print("Found backups:")
+
 for backup in backups {
     print("    ID: \(backup.identifier) Date: \(backup.creationDate)")
 }
-*/
 
-let mostRecentBackup = backups.sorted(by: { $0.creationDate > $1.creationDate }).first!
-if api.connectChatStorageDb(from: mostRecentBackup) {
-    if let chats = api.getChats(from: mostRecentBackup) {
+guard let mostRecentBackup = backups.sorted(by: { $0.creationDate > $1.creationDate }).first else {
+    print("No backups available")
+    exit(1)
+}
 
-        // Encoding chats to JSON
-        let jsonEncoder = JSONEncoder()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        jsonEncoder.dateEncodingStrategy = .formatted(formatter)
-        jsonEncoder.outputFormatting = .prettyPrinted // Optional: if you want the JSON output to be indented
+guard api.connectChatStorageDb(from: mostRecentBackup) else {
+    print("Failed to connect to the most recent backup")
+    exit(1)
+}
+
+let chats = api.getChats(from: mostRecentBackup)
+
+guard !chats.isEmpty  else {
+    print("No chats")
+    exit(1)
+}
+
+print("Most recent backup: \(mostRecentBackup.creationDate)")
+
+// Encoding chats to JSON
+let jsonEncoder = JSONEncoder()
+let formatter = DateFormatter()
+formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+jsonEncoder.dateEncodingStrategy = .formatted(formatter)
+jsonEncoder.outputFormatting = .prettyPrinted // Optional: if you want the JSON output to be indented
+
+do {
+    let jsonData = try jsonEncoder.encode(chats)
+    if let jsonString = String(data: jsonData, encoding: .utf8) {
         do {
-            let jsonData = try jsonEncoder.encode(chats)
-
-            let jsonString = String(data: jsonData, encoding: .utf8)
-            print(jsonString ?? "Failed to convert JSON data to string")
+            try jsonString.write(toFile: "chats.json", atomically: true, encoding: .utf8)
+            print("Info about \(chats.count) chats saved to file chats.json")
         } catch {
-            print("Failed to encode chats to JSON: \(error)")
+            print("Failed to save chats info: \(error)")
         }
     } else {
-        print("No chats")
+        print("Failed to convert JSON data to string")
     }
-} else {
-    print("Failed to connect to the most recent backup")
+} catch {
+    print("Failed to encode chats to JSON: \(error)")
 }
 
