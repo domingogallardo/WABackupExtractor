791d37a75b8e394fc42e36f33bbd9937146c5172
Domingo Gallardo <domingo.gallardo@ua.es>
2023-08-22 18:23:37 +0200

    New version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/domingogallardo/SwiftWABackupAPI.git", 
-            from: "0.9.0"
+            from: "0.10.0"
         ),
     ],
     targets: [
